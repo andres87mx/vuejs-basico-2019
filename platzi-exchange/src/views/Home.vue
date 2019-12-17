e557c78f133605be348609e649aa77127e2d37bc
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-12-17 11:15:15 -0600

    ntroducción a Fetch y API de Coincap

--- platzi-exchange/src/views/Home.vue
+++ platzi-exchange/src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <px-assets-table></px-assets-table>
+    <px-assets-table v-bind:assets="assets"></px-assets-table>
   </div>
 </template>
 <script>
+import api from "@/api";
 import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "Home",
   components: {
     PxAssetsTable
+  },
+  data() {
+    return {
+      assets: []
+    };
+  },
+  created() {
+    api.getAssets().then(assets => (this.assets = assets));
   }
 };
 </script>
